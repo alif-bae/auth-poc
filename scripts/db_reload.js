7f1f1b669698b148e733a2cee445c3b01ece9e55
alif <ali.fawad@readyeducation.com>
2020-12-27 18:31:00 +0500

    fix script to setup and reload db

--- scripts/db_reload.js
+++ scripts/db_reload.js
@@ -1,6 +1,21 @@
-const cmd = require('node-cmd')
+const exec = require('await-exec')
 
-// drop db
-cmd.run('rm ../development.db.sqlite', (err, data, stderr) => {
-    console.log(stderr)
-})
+
+async function reload_database() {
+    const drop_db_cmd = 'rm development.db.sqlite'
+    const db_migrate_cmd = 'npx sequelize db:migrate'
+    const db_seed_cmd = 'npx sequelize --seeders-path seeders/ db:seed:all'
+
+    console.log('Drop DB...')
+    await exec(drop_db_cmd)
+
+    console.log('Run Migrations...')
+    await exec(db_migrate_cmd)
+
+    console.log('Seed DB...')
+    await exec(db_seed_cmd)
+
+    console.log('Success!')
+}
+
+reload_database()
